dd8722a02db14a0c3af86b0a303a5627a80c419c
AhfricanAce <ahfricanace@gmail.com>
2026-09-18 13:27:30 +0100

    Locking Creation So Only Authenticated Users Can Add Them

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryListCreateView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
