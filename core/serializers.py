f56af4c2502ad6c494bbda93c978e88e6084e834
AhfricanAce <ahfricanace@gmail.com>
2026-09-18 14:38:20 +0100

    Fixed a Minor Typo in fields element

--- core/serializers.py
+++ core/serializers.py
@@ -4,5 +4,5 @@ from .models import Category, Course
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug', 'descriptive']
+        fields = ['id', 'name', 'slug', 'description']
         read_only_fields = ['slug'] # Handled automatically by the model save method
