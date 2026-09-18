cf93a4ccd6aeb5c9318c6e1512d8ed2e1cf37d7d
AhfricanAce <ahfricanace@gmail.com>
2026-09-18 14:21:13 +0100

    Fixed few(3) bugs in models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 
 class User(AbstractUser):
     class Roles(models.TextChoices):
@@ -15,8 +16,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     # Use email instead of username for authentication:
-    #USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['username']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
 class InstructorProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='instructor_profile')
@@ -35,4 +36,45 @@ class StudentProfile(models.Model):
     learning_progress = models.IntegerField(default=0)  # Percentage tracker:
 
     def __str__(self):
-        return f"Student: {self.user.get_full_name() or self.user.username}"    
+        return f"Student: {self.user.get_full_name() or self.user.username}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify (self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    class CourseLevels(models.TextChoices):
+        BEGINNER = 'BEGINNER', 'Beginner'
+        INTERMEDIATE = 'INTERMEDIATE', 'Intermediate'
+        ADVANCED = 'ADVANCED', 'Advanced'
+
+    class CourseStatus(models.TextChoices):
+        DRAFT = 'DRAFT', 'Draft'
+        PUBLISHED = 'PUBLISHED', 'Published'
+        ARCHIVED = 'ARCHIVED', 'Archived'
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='courses')
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authored_courses')
+    duration = models.IntegerField(help_text="Duration in hours")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    level = models.CharField(max_length=20, choices=CourseLevels.choices, default=CourseLevels.BEGINNER)
+    status = models.CharField(max_length=20, choices=CourseStatus.choices, default=CourseStatus.DRAFT)
+    thumbnail = models.ImageField(upload_to='course_thumbnails/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
