abe5b89801bc7aaf963685671e9ea5fae2f52cf3
AhfricanAce <ahfricanace@gmail.com>
2026-09-19 08:28:13 +0100

    Setup Register Endpoint

--- config/urls.py
+++ config/urls.py
@@ -20,12 +20,17 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from core.views import CategoryListCreateView
+from core.views import RegisterView, CategoryListCreateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth Endpoints from PRD
+    path('api/auth/register/', RegisterView.as_view(), name='auth_register'),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
