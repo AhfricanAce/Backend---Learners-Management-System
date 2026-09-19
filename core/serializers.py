52b37c4c7dcb0c13082923d613a618ec4d0170df
AhfricanAce <ahfricanace@gmail.com>
2026-09-19 07:49:34 +0100

    Update Sign up Profile in Database To Accomodate 'role' Field

--- core/serializers.py
+++ core/serializers.py
@@ -6,3 +6,30 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'name', 'slug', 'description']
         read_only_fields = ['slug'] # Handled automatically by the model save method
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'role']
+
+    def create(self, validated_data):
+        # Extracts role configuration, defaults to STUDENT is empty:
+        role = validated_data.get('role', User.Roles.STUDENT)
+
+        # Create user instance with securely encrypted password hashing:
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=role
+        )
+
+        # Automatically spin up the corresponding secondary profile wrapper:
+        if user.role == User.Roles.INSTRUCTOR:
+            InstructorProfile.objects.create(user=user)
+        else:
+            StudentProfile.objects.create(user=user)
+
+        return user
