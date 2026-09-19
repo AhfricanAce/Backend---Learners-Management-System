d71e385e3259354841c2910c9ab512b5211ee0ab
AhfricanAce <ahfricanace@gmail.com>
2026-09-19 08:07:34 +0100

    Handle Public Sign-up Traffic

--- core/views.py
+++ core/views.py
@@ -3,8 +3,27 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Category
 from .serializers import CategorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
 
 class CategoryListCreateView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+class RegisterView(APIView):
+    # AllowAny ensures guests can hit this endpoint to create an account:
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "User registered successfully!"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
