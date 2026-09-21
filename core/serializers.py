8d298f309c50ad77e5e965cc79eb9c88ba3906cc
AhfricanAce <ahfricanace@gmail.com>
2026-09-21 01:41:17 +0100

    Implemented a nested feature to instantly display all lessons in a single API call

--- core/serializers.py
+++ core/serializers.py
@@ -1,11 +1,40 @@
 from rest_framework import serializers
 from .models import User, Category, Course, StudentProfile, InstructorProfile, Lesson
 
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = [
+            'id', 'course',
+            'title', 'description',
+            'video_url', 'file_attachment',
+            'order', 'created_at'
+        ]
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'slug', 'description']
         read_only_fields = ['slug'] # Handled automatically by the model save method
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    instructor_name = serializers.CharField(source='instructor.username', read_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    # NESTED RELATIONSHIP: Desgined to pull all lessons connected to this course!:
+    # 'lessons' matches the related_name we set in models.py
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id', 'title','description', 'category', 'category_name', 'instructor',
+            'instructor_name', 'duration', 'price', 'level',
+            'status', 'thumbnail', 'lessons', 'created_at'
+        ]
+        read_only_fields = ['instructor', 'created_at']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -33,26 +62,3 @@ class RegisterSerializer(serializers.ModelSerializer):
             StudentProfile.objects.create(user=user)
 
         return user
-
-class CourseSerializer(serializers.ModelSerializer):
-    instructor_name = serializers.CharField(source='instructor.username', read_only=True)
-    category_name = serializers.CharField(source='category.name', read_only=True)
-
-    class Meta:
-        model = Course
-        fields = [
-            'id', 'title', 'description', 'category', 'category_name',
-            'instructor', 'instructor_name', 'duration', 'price',
-            'level', 'status', 'thumbnail', 'created_at'
-        ]
-        read_only_fields = ['instructor', 'created_at']
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = [
-            'id', 'course',
-            'title', 'description',
-            'video_url', 'file_attachment',
-            'order', 'created_at'
-        ]
