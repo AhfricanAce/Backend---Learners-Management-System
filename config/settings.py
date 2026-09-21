d314afe94d3a550e95321a278f283ac2069a631a
AhfricanAce <ahfricanace@gmail.com>
2026-09-21 01:53:07 +0100

    Added default filter backend

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    # For search and filtering...
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ),
 }
 
 MIDDLEWARE = [
