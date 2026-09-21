4863724ce3d2c57fdb496caf6bf98ee27c9295aa
AhfricanAce <ahfricanace@gmail.com>
2026-09-21 17:43:14 +0100

    Registered tables and quizes to show up in the dashboard

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, InstructorProfile, StudentProfile
+from .models import User, InstructorProfile, StudentProfile, Category, Course, Lesson, Quiz, QuizQuestion, QuizAttempt
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
