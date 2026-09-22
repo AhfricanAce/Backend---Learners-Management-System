ac3153b7cd998c42a0dbecc6280e8f02702a5c75
AhfricanAce <ahfricanace@gmail.com>
2026-09-22 02:59:45 +0100

    Registered swagger in settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Documentation Engine
+    'drf_yasg',
+
+    # Core Apps
     'rest_framework',
     'core',
 ]
